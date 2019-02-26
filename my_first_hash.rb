a81f255660fdf3462b88757856be4c142db0265e
A Belgrave <abelgrave95@gmail.com>
2019-02-26 21:32:42 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -21,6 +21,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+  shipping_manifest["oil paintings"]
 
 end
 
@@ -31,13 +32,10 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
-
-
+  shipping_manifest["muskets"] = 2
   # add 4 gun powder to the shipping_manifest hash below
-
-
-
+  shipping_manifest["gun powder"] =4
   # return the shipping_manifest hash below
+  shipping_manifest
 
 end
